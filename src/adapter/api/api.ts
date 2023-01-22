90a820f7c2db76757d8a3d375c24c35662ad8ddf
Achmad Ali Baidlowi <ali.baidlowi@majoo.id>
2023-01-22 15:37:56 +0700

    refactor: eject api adapter from main system
    this action to reduce avoid ambigous system architecture that only used for api server

---
+++ src/adapter/api/api.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/api/check-health", (req: Request, res: Response) => {
+  res.send({
+    message: "server is up",
+    data: null,
+  });
+});
+
+export default () => {
+  app.listen(3000, () => {
+    console.log("server is running");
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import express, {Express, json, Request, Response} from 'express'
-import dotenv from 'dotenv'
+import apiAdapter from './adapter/api/api';
 
-dotenv.config()
-
-const app  = express()
-
-app.get("/api/check-health", (req: Request, res: Response) => {
-    res.send({
-        message: "server is up",
-        data: null
-    })
-})
-
-
-app.listen(3000, ()=> {
-    console.log("server is running")
-})
+apiAdapter();
